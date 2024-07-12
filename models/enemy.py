ca5be3176fe9238caf42812a66ab8f6f397b066b
sa <sa>
2024-07-12 19:18:35 +0300

    enemy + base

--- models/enemy.py
+++ models/enemy.py
@@ -1,4 +1,4 @@
-class Enemy:
+class EnemyPoint:
     def __init__(self, attack, health, is_head, last_attack, name, x, y):
         self.attack = attack
         self.health = health 
@@ -10,8 +10,8 @@ class Enemy:
         self.y = y
 
 
-def enemy_from_json(enemy_data: dict) -> Enemy:
-    return Enemy(
+def enemy_points_from_json(enemy_data: dict) -> EnemyPoint:
+    return EnemyPoint(
         enemy_data['attack'],
         enemy_data['health'],
         enemy_data['isHead'],
@@ -20,3 +20,12 @@ def enemy_from_json(enemy_data: dict) -> Enemy:
         enemy_data['x'],
         enemy_data['y']
     )
+
+class Enemy:
+    def __init__(self, enemy_points: list[EnemyPoint]):
+        self.enemy_points = enemy_points
+
+
+def enemy_from_json(enemy_data: list) -> Base:
+     points = [enemy_points_from_jsone(i) for i in enemy_data]
+     return Enemy(points)
