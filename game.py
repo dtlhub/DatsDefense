4d7c2c891e18aa02e937ad1ed91e0996ee77f04a
jewelofchaos9 <jewelofchaos9@gmail.com>
2024-10-08 10:14:09 +0000

    buttons

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import pygame
 import time
 import sys
+from dataclasses import dataclass
 from random import randint
 
 BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
+WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
+GRAY = (128, 128, 128)
+GREEN = (0, 255, 255)
+
+
 WINDOW_HEIGHT = 1000
 WINDOW_WIDTH = 1300
 
@@ -16,14 +21,19 @@ CAMERA_COORD_Y = 0
 CAMERA_SPEED = 0.1
 CAMERA_DELTA_X = 0
 CAMERA_DELTA_Y = 0
+# for spped of camera
 CAMERA_PING = 0.01
+LAST_PRESSED_K_DOWN = time.time()
+LAST_PRESSED_K_UP = time.time()
+LAST_PRESSED_K_LEFT = time.time()
+LAST_PRESSED_K_RIGHT = time.time()
 SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
-ZOOM = 0.3
 
 # one move of mouse produces 1
 # zoom in or zoom out with coef 1/ZOOM_SCALE
-ZOOM_SCALE = 10
+ZOOM = 0.3
+ZOOM_SCALE = 100
 BLOCK_SIZE = 20
 SPRITE_SIZE = BLOCK_SIZE
 
@@ -34,6 +44,21 @@ ZOOM_CENTERED_ENABLED = False
 MENU_X = 200
 pygame.font.init()
 FONT = pygame.font.SysFont('JetBrains Mono', 20)
+@dataclass
+class Button:
+    name: str
+    x_left: int
+    y_top: int
+    width: int
+    height: int
+    # is button checkbox
+    supposed_to_be_active: bool
+    is_active: bool
+BUTTONS = [
+    Button('Increase round', 10, 180, 180, 30, False, False),
+    Button('Decrease round', 10, 220, 180, 30, False, False),
+    Button('Test active', 10, 260, 180, 30, True, False),
+]
 
 
 # GAME_INFO assume we renew it every round
@@ -63,63 +88,78 @@ class Player(pygame.sprite.Sprite):
 
 def main():
     global SCREEN, CLOCK, CAMERA_COORD_X, CAMERA_COORD_Y, ZOOM, CAMERA_DELTA_X, CAMERA_DELTA_Y, CAMERA_PING, PLAYERS
-    LAST_PRESSED_K_DOWN = time.time()
-    LAST_PRESSED_K_UP = time.time()
-    LAST_PRESSED_K_LEFT = time.time()
-    LAST_PRESSED_K_RIGHT = time.time()
     pygame.init()
 
     while True:
         SCREEN.fill(BLACK)
         draw_grid()
         draw_text()
+        draw_buttons()
         for event in pygame.event.get():
-            """
-            if event.type == pygame.KEYDOWN:
-                if event.key == ord('a'):
-                    CAMERA_COORD_X -= 1
-                if event.key == ord('s'):
-                    # want camera to move down, so invert
-                    CAMERA_COORD_Y += 1
-                if event.key == ord('d'):
-                    CAMERA_COORD_X += 1
-                if event.key == ord('w'):
-                    CAMERA_COORD_Y -= 1
-            """
-
             if event.type == pygame.MOUSEWHEEL:
-                # event.y shows up or down
-                x, y = pygame.mouse.get_pos()
-                ZOOM += -event.y / 10
-                if ZOOM_CENTERED_ENABLED:
-                    camera_coords = mouse_coord_to_grid(x, y)
-                    CAMERA_COORD_X = camera_coords[0]
-                    CAMERA_COORD_Y = camera_coords[1]
-                for p in PLAYERS:
-                    p.update_sprite()
+                handle_mouse_wheel(event)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                handle_mouse_button(event)
 
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        now = time.time()
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and (now - LAST_PRESSED_K_LEFT > CAMERA_PING):
-            CAMERA_COORD_X += 1
-            LAST_PRESSED_K_LEFT = now
-        if keys[pygame.K_RIGHT] and now - LAST_PRESSED_K_RIGHT > CAMERA_PING:
-            CAMERA_COORD_X -= 1
-            LAST_PRESSED_K_RIGHT = now
-        if keys[pygame.K_UP] and now - LAST_PRESSED_K_UP > CAMERA_PING:
-            CAMERA_COORD_Y += 1
-            LAST_PRESSED_K_UP = now
-        if keys[pygame.K_DOWN] and now - LAST_PRESSED_K_DOWN > CAMERA_PING:
-            CAMERA_COORD_Y -= 1
-            LAST_PRESSED_K_DOWN = now
-        for p in PLAYERS:
-            p.update()
-            draw_player(p)
+        handle_button_pressed(keys)
         pygame.display.update()
+
+
+def handle_mouse_wheel(event):
+    global ZOOM, ZOOM_SCALE, ZOOM_CENTERED_ENABLED, PLAYERS, CAMERA_COORD_X, CAMERA_COORD_Y
+    x, y = pygame.mouse.get_pos()
+    ZOOM += -event.y / ZOOM_SCALE
+    if ZOOM_CENTERED_ENABLED:
+        camera_coords = mouse_coord_to_grid(x, y)
+        CAMERA_COORD_X = camera_coords[0]
+        CAMERA_COORD_Y = camera_coords[1]
+    for p in PLAYERS:
+        p.update_sprite()
+
+
+def handle_button_pressed(keys):
+    global LAST_PRESSED_K_LEFT, LAST_PRESSED_K_RIGHT, LAST_PRESSED_K_UP, LAST_PRESSED_K_DOWN
+    global CAMERA_PING, PLAYERS, CAMERA_COORD_Y, CAMERA_COORD_X
+
+    now = time.time()
+    if keys[pygame.K_LEFT] and (now - LAST_PRESSED_K_LEFT > CAMERA_PING):
+        CAMERA_COORD_X += 1
+        LAST_PRESSED_K_LEFT = now
+    if keys[pygame.K_RIGHT] and now - LAST_PRESSED_K_RIGHT > CAMERA_PING:
+        CAMERA_COORD_X -= 1
+        LAST_PRESSED_K_RIGHT = now
+    if keys[pygame.K_UP] and now - LAST_PRESSED_K_UP > CAMERA_PING:
+        CAMERA_COORD_Y += 1
+        LAST_PRESSED_K_UP = now
+    if keys[pygame.K_DOWN] and now - LAST_PRESSED_K_DOWN > CAMERA_PING:
+        CAMERA_COORD_Y -= 1
+        LAST_PRESSED_K_DOWN = now
+    for p in PLAYERS:
+        p.update()
+        draw_player(p)
+
+
+def handle_mouse_button(event):
+    global BUTTONS, ROUND
+    x, y = pygame.mouse.get_pos()
+
+    def in_button(button: Button, x: int, y: int):
+        return (button.x_left <= x <= button.width) and (button.y_top <= y <= button.y_top + button.height)
+
+    for button in BUTTONS:
+        if in_button(button, x, y):
+            if button.name == 'Increase round':
+                ROUND += 1
+            if button.name == 'Decrease round':
+                ROUND -= 1
+            if button.name == 'Test active':
+                button.is_active ^= 1
 
 
 def mouse_coord_to_grid(x: int, y: int):
@@ -175,7 +215,7 @@ def draw_player(player: Player):
 
 
 def draw_grid():
-    block_size = int(BLOCK_SIZE // ZOOM) #Set the size of the grid block
+    block_size = int(BLOCK_SIZE // ZOOM)
     x_bound, y_bound = compute_screen_bounds()
     for x in range(MENU_X, WINDOW_WIDTH, block_size):
         for y in range(0, WINDOW_HEIGHT, block_size):
@@ -207,4 +247,23 @@ def draw_text():
 
     text_surface = FONT.render(f'{BASE_HEALTH} / {MAX_BASE_HEALT}', False, color)
     SCREEN.blit(text_surface, (10, 130))
+
+
+def draw_buttons():
+    global BUTTONS
+    for button in BUTTONS:
+        if button.supposed_to_be_active:
+            if button.is_active:
+                bg_color = GREEN
+            else:
+                bg_color = RED
+        else:
+            bg_color = GRAY
+
+        rect = pygame.Rect(button.x_left, button.y_top, button.width, button.height)
+        pygame.draw.rect(SCREEN, bg_color, rect, 0)
+        text_surface = FONT.render(button.name, False, WHITE)
+        SCREEN.blit(text_surface, (button.x_left, button.y_top))
+
+
 main()
